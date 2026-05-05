51194a5ceedb36976de0212c1db5c1c5e3cf18af
denis22078533 <denis22078533@gmail.com>
2026-05-05 18:29:09 +0000

    oofjvfj

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: './',
   plugins: [
     react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'backend/api',
+          dest: '' 
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
