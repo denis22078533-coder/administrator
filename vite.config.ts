27697638cc3adbd70463bc7295bf4233c020b53b
denis22078533 <denis22078533@gmail.com>
2026-05-10 18:11:44 +0000

    fix: handle spa routing and 404s with vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    hmr: {
-      clientPort: 443
-    }
-  },
+  appType: 'spa',
 })
