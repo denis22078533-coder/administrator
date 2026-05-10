736bd7f07fbcbe4d2c93f7027063c837cefb9086
denis22078533 <denis22078533@gmail.com>
2026-05-10 12:51:21 +0000

    76746747

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const redirectPath = sessionStorage.getItem('redirect');
+if (redirectPath) {
+  sessionStorage.removeItem('redirect');
+  if (redirectPath !== window.location.pathname) {
+    window.history.replaceState(null, '', redirectPath);
+  }
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
